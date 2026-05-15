31d87c8eb8a931772dd60d979c5582f9593418f4
564448 <knight.wintawit@gmail.com>
2026-05-15 09:21:38 +0700

    add import api key

--- CLI.py
+++ CLI.py
@@ -1,8 +1,9 @@
 from Fetch import Fetch
 from Analytic import analyze_and_export_csv  as analyze
 from Extract import get_comments_from_csv as get_comment
+from get_api_key import get_api_key as get_api
 
-API_KEY = ""
+API_KEY = get_api("API_Key.txt")
 Youtube_link = input("Youtube Link\n:")
 file_name,total_comment = Fetch(API_KEY,Youtube_link)
 comment_list = get_comment(file_name)
