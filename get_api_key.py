4206a721b80991cb7eb66cef9e360380d20bf5de
564448 <knight.wintawit@gmail.com>
2026-05-31 19:22:10 +0700

    add try-except

--- get_api_key.py
+++ get_api_key.py
@@ -1,7 +1,9 @@
 def get_api_key(filename):
-    with open(filename,mode='r') as file:
-        line = file.readline()
-    return line
+    try:
+        with open(filename, "r") as f:
+            return f.readline().strip()
+    except FileNotFoundError:
+        return None
     
 if __name__ == "__main__" :
     print(get_api_key("API_Key.txt"))
